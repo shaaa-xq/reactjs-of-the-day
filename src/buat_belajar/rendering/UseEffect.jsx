3a0b418a9ca7db87a0371611eba2d932351202e1
shaaa-xq <ayeshacha177@gmail.com>
2025-01-17 22:55:12 +0700

    useeffect rendering part 1

--- src/buat_belajar/rendering/UseEffect.jsx
+++ src/buat_belajar/rendering/UseEffect.jsx
@@ -1,19 +1,55 @@
 import { useState, useEffect } from "react";
 
-// with dependency
-function WithDependency(){
-  const [count, setCount] = useState(0);
+// import '../..index.css'
 
+// with dependency
+// function WithDependency(){
+//   const [count, setCount] = useState(0);
+
+//   useEffect(() => {
+//     console.log(`Count updated: ${count}`)
+//   }, [count]);
+
+//   return(
+//     <div className="grid place-content-center mt-40">
+//       <button className="rounded-full bg-sky-400" onClick={() => setCount(count + 1)}>Count</button>
+//       <p>Count: {count}</p>
+//     </div>
+//   )
+// }
+
+// export default WithDependency;
+
+// without dependency
+function WithoutDependency(){
+  
   useEffect(() => {
-    console.log(`Count updated: ${count}`)
-  }, [count]);
+    console.log('Komponen ini dirender sekali')
+  }), [];
 
   return(
-    <div className="di">
-      <button onClick={() => setCount(count + 1)}>Count</button>
-      <p>Count: {count}</p>
-    </div>
-  )
+    <h1>Komponen ini hanya dirender sekali!</h1>
+  );
 }
 
-export default WithDependency;
+export default WithoutDependency;
+
+// cleanup effect
+// function CleanupEffect(){
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       console.log("Running interval...")
+//     }, 1000);
+
+//     return() => {
+//       clearInterval(interval);
+//       console.log("Interval has been cleaned!")
+//     }
+//   }, []);
+
+//   return(
+//     <h3>Check console to see clean up effect!</h3>
+//   );
+// }
+
+// export default CleanupEffect;
--- src/buat_belajar/rendering/main.jsx
+++ src/buat_belajar/rendering/main.jsx
@@ -1,9 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
 import WithDependency from './UseEffect'
+import WithoutDependency from './UseEffect'
+import CleanupEffect from './UseEffect'
+// import '../../index.css'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <WithDependency />
+    {/* <WithDependency /> */}
+    <WithoutDependency />
+    {/* <CleanupEffect /> */}
   </StrictMode>
 );
